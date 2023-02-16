a932f530677e33d4c71e14658f786597cc83544a
Edison3238 <9thebigez9@gmail.com>
2023-02-15 22:48:59 -0500

    aa

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -8,10 +8,10 @@ const midelware = require('../midelware/token.midelware')
 const route = Router();
 
 
-route.get('/cliente', midelware.protegerRuta, clienteController.getCliente);
-route.post('/cliente', midelware.protegerRuta, clienteController.postCreateCliente);
-route.put('/cliente', midelware.protegerRuta, clienteController.putUpdateCliente);
-route.delete('/cliente', midelware.protegerRuta, clienteController.deleteCliente);
+route.get('/cliente', clienteController.getCliente);
+route.post('/cliente', clienteController.postCreateCliente);
+route.put('/cliente', clienteController.putUpdateCliente);
+route.delete('/cliente', clienteController.deleteCliente);
 
 
 module.exports = route
