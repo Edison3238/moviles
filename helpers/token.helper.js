72a4fe65b87f98caba31f76997744f5a913f99ac
Edison3238 <9thebigez9@gmail.com>
2023-02-15 22:36:08 -0500

    a

--- helpers/token.helper.js
+++ helpers/token.helper.js
@@ -7,7 +7,7 @@ function generarToken(suc_user, suc_pass) {
     };
 
     const token = jwt.sign(payload, 'servidorestoken', {
-        expiresIn: '1h'
+        expiresIn: '10h'
     });
 
     return token;
--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, access-token");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //PUERTO
 const PORT = process.env.PORT || 1999;
 
